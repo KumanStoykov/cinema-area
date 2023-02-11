4d717f257509bbe0771d46852b38e583cefb33aa
Kuman Stoykov <kumanstoykov@gmail.com>
2023-02-11 17:39:37 +0100

    Added search bar

--- src/components/layout/Header/Header.tsx
+++ src/components/layout/Header/Header.tsx
@@ -9,14 +9,21 @@ const Header: React.FC = () => {
         <header className={styles.header}>
             <div className={`${styles['nav-wrap']} ${'container'}`}>
                 <div className={styles.logo}><Link to={'/'}>LOGO</Link></div>
-                <nav className={styles.nav}>
-                    <ul className={styles['nav-list']}>
-                        <li className={styles['nav-list']}><Link to={'/'}>HOME</Link></li>
-                        <li className={styles['nav-list']}><Link to={'/'}>FILMS</Link></li>
-                        <li className={styles['nav-list']}><Link to={'/'}>LOGIN</Link></li>
-                        <li className={styles['nav-list']}><Link to={'/'}>REGISTER</Link></li>
-                    </ul>
-                </nav>
+                <div className={styles['nav-search-wrap']}>
+                    <nav className={styles.nav}>
+                        <ul className={styles['nav-list']}>
+                            <li className={styles['nav-list']}><Link to={'/'}>Login</Link></li>
+                            <li className={styles['nav-list']}><Link to={'/'}>Register</Link></li>
+                            <li className={styles['nav-list']}><Link to={'/'}>My-History</Link></li>
+                        </ul>
+                    </nav>
+                    <div className={styles['search-wrap']}>
+                        <form className={styles['search-form']}>
+                            <input className={styles['search-input']} type='text' />
+                            <button className={styles['search-btn']}>search</button>
+                        </form>
+                    </div>
+                </div>
             </div>
         </header>
     );
